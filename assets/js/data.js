cb8fb02c32e3a83ff9f2a84d5332c28fcec996b4
freetimezp <kraftukrnet@ukr.net>
2024-10-31 00:46:54 +0200

    03. js-portfolio-142-scroll. GSAP animation.

---
+++ assets/js/data.js
@@ -0,0 +1,37 @@
+export const data = [
+    {
+        title: "Nebula Quest",
+        tagline: "Redefining Galactic UX",
+        year: "2023",
+        tag: "game design",
+        link: "https://www.youtube.com"
+    },
+    {
+        title: "Aurora Scribe",
+        tagline: "Crafting Stories in Light",
+        year: "2022",
+        tag: "content creation",
+        link: "https://www.youtube.com"
+    },
+    {
+        title: "Echo Circuit",
+        tagline: "Where sound Meets Code",
+        year: "2024",
+        tag: "audio engineering",
+        link: "https://www.youtube.com"
+    },
+    {
+        title: "Zenith Horizon",
+        tagline: "Breaking Boundries in Motion",
+        year: "2023",
+        tag: "motion development",
+        link: "https://www.youtube.com"
+    },
+    {
+        title: "Prism Architect",
+        tagline: "Sculpting Digital Dreams",
+        year: "2024",
+        tag: "web development",
+        link: "https://www.youtube.com"
+    },
+];
